31767f47a7a0737c58f91a42e23eec5b6eba95bf
Saayan8981 <saayan8981@gmail.com>
2023-02-07 17:10:14 +0530

    commit

--- copy.sh
+++ copy.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-
-
 name='Kevin'
 greet="Welcome $name, to GeeksforGeeks!"
 echo $greet
 
 filename_1='Idea1.txt'
-filename_2='Idea2.txt'
-filename_3='Idea3.txt'
-search=';hkfjshgs;a;R(0,1,1)(4,3,2);bvhjfv;\n_;cncs;0123;nbvhsfbvhjsbvjh vds vsjhbvs;;'
-replace=';hkfjshgs;'
 
-echo $filename_1
+search='hkfjshgs;a;R(0,1,1)(3,2);'
+replace=${filename_1%.txt} + ${';a;'} + ${filename_1%.txt} + ${'_1;;;;'}
+
+echo $filename_1, $replace
 
 cd bkp/
 
@@ -20,13 +17,12 @@ echo "Script executed from: ${PWD} $name $filename_1"
 if [ -e  $filename_1 ]; 
 then
     echo $filename_1
-    sed -i '' -e "s/$search/$replace/" "$filename_1"
+    sed -i -z "s/$search/$replace/" "$filename_1"
     echo "$filename_1 is exist"
 else
     echo "No file exist"
 fi
 
-mv Idea1.txt-e Idea_new.txt
 # if [ -f $filename_2 ]
 # then
 #     sed -i "s/$search/$filename_2/" $filename_2

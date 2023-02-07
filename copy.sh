619dba70b8127def92e67deb6989137cda29c281
Saayan8981 <saayan8981@gmail.com>
2023-02-07 19:29:54 +0530

    commit

--- copy.sh
+++ copy.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
+log="/home/saayan8981/Documents/github/Shell-scripting-code/log/log_file.txt"
 filename="Idea1.txt Idea2.txt Idea3.txt"
 source='bkp/'
 dest='output/'
 search=';bfkbsb;bsvbsvb;R(9,8,0);;\n;sbsd;a;6245865476;bnmvbxcv cxdada;;'
 
-echo $filename
+printf "Log File - " > $log
+date > $log
+echo $filename >> $log
 
-echo "Script executed from: ${PWD} $filename"
+echo "Script executed from: ${PWD} $filename" >> $log
 
 for file in $filename;
 do
     cd $source
-    echo "Script executed from: ${PWD} $file"
+    echo "Script executed from: ${PWD} $file" >> $log
     if [ -e  $file ]; then
-        echo $file
+        echo $file >> $log
         replace=${file%.txt}';a;'${file%.txt}'_1;;;;'
         sed -i -z "s/$search/$replace/" "$file"
-        echo "$file is exist"
+        echo "$file is exist" >> $log
         cd ..
-        echo "back to : ${PWD}"
+        echo "back to : ${PWD}" >> $log
         cp $source'/'$file $dest
     else
-        echo "Script executed from: ${PWD} $file"
-        echo "$file is not exist"
+        echo "Script executed from: ${PWD} $file" >> $log
+        echo "$file is not exist" >> $log
     fi
 done    

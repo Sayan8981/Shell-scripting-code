f02b12adcf907b055386321c35fbe0eddb5d6163
Saayan8981 <saayan8981@gmail.com>
2023-02-07 17:38:26 +0530

    commit

--- copy.sh
+++ copy.sh
@@ -1,40 +1,25 @@
 #!/bin/bash
-name='Kevin'
-greet="Welcome $name, to GeeksforGeeks!"
-echo $greet
 
 filename_1='Idea1.txt'
-
-search='hkfjshgs;a;R(0,1,1)(3,2);'
+source='bkp/'
+dest='output/'
+search='hkfjshgs;a;R(0,1,1)(3,2);\n_;cncs;0123;nbvhsfbvhjsbvjh vds vsjhbvs;;'
 replace=${filename_1%.txt}';a;'${filename_1%.txt}'_1;;;;'
 
 echo $filename_1, $replace
 
-cd bkp/
+cd $source
 
-echo "Script executed from: ${PWD} $name $filename_1"
+echo "Script executed from: ${PWD} $filename_1"
 
 if [ -e  $filename_1 ]; 
 then
     echo $filename_1
     sed -i -z "s/$search/$replace/" "$filename_1"
     echo "$filename_1 is exist"
+    cd ..
+    echo "back to : ${PWD}"
+    cp $source'/'$filename_1 $dest
 else
-    echo "No file exist"
+    echo "$filename_1 is not exist"
 fi
-
-# if [ -f $filename_2 ]
-# then
-#     sed -i "s/$search/$filename_2/" $filename_2
-#     echo "$filename_2 is exist"
-# else
-#     echo "No file exist"
-# fi
-
-# if [ -f $filename_3 ]
-# then
-#     sed -i "s/$search/$filename_3/" $filename_3
-#     "$filename_3 is exist"
-# else
-#     echo "No file exist"
-# fi

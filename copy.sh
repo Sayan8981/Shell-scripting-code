ea36b07231894693eedd440137da526dbf04cd13
Saayan8981 <saayan8981@gmail.com>
2023-02-08 18:29:26 +0530

    commit last

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #log init
-log="/Users/saayandas/Documents/Github_repo/Shell-scripting-code/log/log_file.txt"
+log="/home/saayan8981/Documents/github/Shell-scripting-code/log/log_file.txt"
 #input files
 filename="Idea1.txt Idea2.txt Idea3.txt"
 #source to copy
@@ -11,25 +11,25 @@ dest='output/'
 #search text to replace
 search=';bfkbsb;bsvbsvb;R(9,8,0);;\n;sbsd;a;6245865476;bnmvbxcv cxdada;;'
 
-date > $log
-echo $filename >> $log
+echo "[${USER}][`date`]" >> $log
+echo "[${USER}][`date`] - ${*} $filename" >> $log
 
-echo "Script executed from: ${PWD} $filename" >> $log
+echo "[${USER}][`date`] - ${*} Script executed from: ${PWD} for [$filename]" >> $log
 #operation
 for file in $filename;
 do
     cd $source
-    echo "Script executed from: ${PWD} $file" >> $log
     if [ -e  $file ]; then
-        echo $file >> $log
+        echo "[${USER}][`date`] - ${*} $file" >> $log
         replace=${file%.txt}';a;'${file%.txt}'_1;;;;'
         sed -i -z "s/$search/$replace/" "$file"
-        echo "$file is exist" >> $log
+        echo "[${USER}][`date`] - ${*} $file is exist" >> $log
         cd ..
-        echo "back to : ${PWD}" >> $log
+        echo "[${USER}][`date`] - ${*} back to : ${PWD}" >> $log
         cp $source'/'$file $dest
+        echo "[${USER}][`date`] - ${*} $file copied to $dest" >> $log
     else
-        echo "Script executed from: ${PWD} $file" >> $log
-        echo "$file is not exist" >> $log
+        echo "[${USER}][`date`] - ${*} Script executed from: ${PWD} $file" >> $log
+        echo "[${USER}][`date`] - ${*} $file is not exist" >> $log
     fi
 done
